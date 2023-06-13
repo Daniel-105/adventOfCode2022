214708b34f53007b70a037b64e0ecc73d0f221df
Daniel-105 <danielpinto105@gmail.com>
2023-06-14 00:43:15 +0100

    got the the sample group to work. Trying to figure out how to separate the original array now

--- day1/app.js
+++ day1/app.js
@@ -14,26 +14,47 @@ const sampleCode = `1000
 10000`;
 
 // Spliting the initial string into an array with multiple strings
-let strArray1 = sampleCode.split("\n");
+let strArray1 = sampleCode.split("\n\n");
+
+// taking the first group as an example
+let sampleGroup = strArray1[0];
+
+for (let i = 0; i < strArray1.length; i++) {
+  strArray1[i];
+}
+
+const getSumOfGroup = (group) => {
+  const numbList = group
+    // Spliting the group by the \n
+    // Still keeping the numbers as strings
+    .split("\n")
+
+    // turning each string into number
+    .map((number) => Number(number))
+
+    // Calculating the sum of the group
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+  return numbList;
+};
+
+console.log(strArray1);
+console.log(getSumOfGroup(sampleGroup));
 
 // Slicing the array to see the first elf
-let firstElf = strArray1.slice(0, strArray1.indexOf(""));
+// let firstElf = strArray1.slice(0, strArray1.indexOf(""));
 
 // let numbersArray = strArray1.map((number) => Number(number));
 
 // Turning the array of strings into an array of numbers
-let firstElfNumbers = firstElf.map((number) => Number(number));
+// let firstElfNumbers = firstElf.map((number) => Number(number));
 
 // Calculating the sum o values
-let sumOfCalories = firstElfNumbers.reduce(
-  (accumulator, currentValue) => accumulator + currentValue
-);
+// let sumOfCalories = firstElfNumbers.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue
+// );
 
 // console.log(numbersArray);
 // console.log(firstElf);
-console.log(firstElfNumbers);
-console.log(sumOfCalories);
-console.log(strArray1);
 
 // O que é que eu quero fazer agora?
 // Somar os numeros.
