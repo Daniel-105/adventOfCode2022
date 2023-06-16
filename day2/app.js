e7537e44b53641b08b0968a5ebf80aac1d380981
Daniel-105 <danielpinto105@gmail.com>
2023-06-16 21:37:38 +0100

    almost done with sample input. There are some undefineds I don't understand yet

--- day2/app.js
+++ day2/app.js
@@ -16,6 +16,55 @@ C Z`;
 // Y = Paper
 // Z = Scissors
 
+const win = (player1, player2) => {
+  if (
+    (player1 === scissors[0] || player1 === scissors[1]) &&
+    (player2 === scissors[0] || player2 === scissors[1])
+  ) {
+    console.log("Draw");
+  } else if (
+    (player1 === scissors[0] || player1 === scissors[1]) &&
+    (player2 === rock[0] || player2 === rock[1])
+  ) {
+    console.log("Player 2 wins");
+  } else if (
+    (player1 === scissors[0] || player1 === scissors[1]) &&
+    (player2 === paper[0] || player2 === paper[1])
+  ) {
+    console.log("Player 1 wins");
+  } else if (
+    (player1 === rock[0] || player1 === rock[1]) &&
+    (player2 === paper[0] || player2 === paper[1])
+  ) {
+    console.log("Player 2 wins");
+  } else if (
+    (player1 === rock[0] || player1 === rock[1]) &&
+    (player2 === rock[0] || player2 === rock[1])
+  ) {
+    console.log("Draw");
+  } else if (
+    (player1 === rock[0] || player1 === rock[1]) &&
+    (player2 === scissors[0] || player2 === scissors[1])
+  ) {
+    console.log("Player 1 wins");
+  } else if (
+    (player1 === paper[0] || player1 === paper[1]) &&
+    (player2 === scissors[0] || player2 === scissors[1])
+  ) {
+    console.log("Player 2 wins");
+  } else if (
+    (player1 === paper[0] || player1 === paper[1]) &&
+    (player2 === paper[0] || player2 === paper[1])
+  ) {
+    console.log("Draw");
+  } else if (
+    (player1 === paper[0] || player1 === paper[1]) &&
+    (player2 === rock[0] || player2 === rock[1])
+  ) {
+    console.log("Player 1 wins");
+  }
+};
+
 const rock = ["A", "X"];
 const paper = ["B", "Y"];
 const scissors = ["C", "Z"];
@@ -24,9 +73,18 @@ const rounds = sampleInput.split("\n").map((round) => round.split(" "));
 
 console.log(rounds);
 
-if (
-  rounds[0][0] === scissors[0] ||
-  (rounds[0][0] === scissors[1] && rounds[0][1] === scissors[0]) ||
-  rounds[0][1] === scissors[1]
-)
-  console.log("Draw");
+// rounds.forEach(win());
+// rounds.map((round) => win(round[0], round[1]));
+
+//callbackfn [ , thisArg ] )
+
+// win(rounds[0][0], rounds[0][1]);
+
+console.log(rounds.map((round) => win(round[0], round[1])));
+
+// if (
+//   rounds[0][0] === scissors[0] ||
+//   (rounds[0][0] === scissors[1] && rounds[0][1] === scissors[0]) ||
+//   rounds[0][1] === scissors[1]
+// )
+//   console.log("Draw");
